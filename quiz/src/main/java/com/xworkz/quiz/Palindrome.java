2ea75ad1e8a5ebac7735896e1eb14655f8d49460
MonishaDR-25 <monishadr25@gmail.com>
2025-08-23 19:14:07 +0530

    Ninja:Quiz questions

---
+++ quiz/src/main/java/com/xworkz/quiz/Palindrome.java
@@ -0,0 +1,24 @@
+package com.xworkz.quiz;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num, reversed = 0;
+        while (num > 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+        sc.close();
+    }
+}
